2fcb8ad0fd42c67897ffb20a0e6ea39448e641c5
bastienwcs <email>
2019-10-18 13:08:28 +0200

    init

---
+++ src/main/java/com/wildcodeschool/giftmefive/GiftMeFiveApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.giftmefive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftMeFiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftMeFiveApplication.class, args);
+	}
+
+}
