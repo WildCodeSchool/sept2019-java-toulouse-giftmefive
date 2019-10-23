a7dc264ac97459fcf537b506e212808e0d4211ca
Marine Mombereau <Marinemombereau@gmail.com>
2019-10-23 11:09:45 +0200

    lien thymeleaf signIn et signUp

--- src/main/java/com/wildcodeschool/giftmefive/GiftMeFiveApplication.java
+++ src/main/java/com/wildcodeschool/giftmefive/GiftMeFiveApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GiftMeFiveApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GiftMeFiveApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GiftMeFiveApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/wildcodeschool/giftmefive/controller/signUpController.java
@@ -0,0 +1,12 @@
+package com.wildcodeschool.giftmefive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class signUpController {
+    @GetMapping("/signUp")
+    public String callSignIn() {
+        return "signUp.html";
+    }
+}
