21f1e8a794d590f35b5c20e35d6baa3c38558a35
Marine Mombereau <Marinemombereau@gmail.com>
2019-10-24 10:08:58 +0200

    descriptionList sans e

--- src/main/java/com/wildcodeschool/giftmefive/controller/DescriptionListController.java
+++ src/main/java/com/wildcodeschool/giftmefive/controller/DescriptionListController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class DescriptionListController {
     @GetMapping("/descriptionList")
-    public String descriptionListe() {
+    public String descriptionList() {
         return "descriptionList";
     }
 }
