242e49212d439422162b693457f4ff72c963c0b0
Marine Mombereau <Marinemombereau@gmail.com>
2019-10-24 10:03:02 +0200

    descriptionlistecontroller route sans e

--- src/main/java/com/wildcodeschool/giftmefive/controller/DescriptionListController.java
+++ src/main/java/com/wildcodeschool/giftmefive/controller/DescriptionListController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DescriptionListController {
-    @GetMapping("/descriptionListe")
+    @GetMapping("/descriptionList")
     public String descriptionListe() {
         return "descriptionListe";
     }
