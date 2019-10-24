df113a80861083ab056952532600d4a4fd539a47
Marie Vendran <marie.vendran@gmail.com>
2019-10-24 16:47:15 +0200

    controller added for giftList and lists, change requested resolved

--- src/main/java/com/wildcodeschool/giftmefive/controller/Controllers.java
+++ src/main/java/com/wildcodeschool/giftmefive/controller/Controllers.java
@@ -1,6 +1,7 @@
 package com.wildcodeschool.giftmefive.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,6 +10,16 @@ public class Controllers {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/lists")
+    public String list() {
+        return "lists";
+    }
+
+    @GetMapping("/giftList")
+    public String giftList() {
+        return "giftList";
     }
 }
 
