0e5dc574a69367ca34ea00b5113304772352e3fc
jerome valette <valettejerome31@gmail.com>
2019-10-25 12:05:54 +0200

    correction conflits

--- src/main/java/com/wildcodeschool/giftmefive/controller/FriendsViewController.java
+++ src/main/java/com/wildcodeschool/giftmefive/controller/FriendsViewController.java
@@ -4,17 +4,18 @@ import com.wildcodeschool.giftmefive.repository.ListGiftsRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FriendsViewController {
 
-    @GetMapping("/friendsview")
-    public String friendsView(Model out, @RequestParam(name = "idListe", defaultValue = "1", required = false) Long id) {
+    @GetMapping("/liste-partage")
+    public String friendsView(Model out, @RequestParam(name = "id-liste", defaultValue = "1", required = false) Long id) {
         GiftsRepository repository = new GiftsRepository();
         ListGiftsRepository repo =new ListGiftsRepository();
         out.addAttribute("gifts", repository.findByListGiftsId(id));
         out.addAttribute("nameList",repo.findById(id));
-        return "friendsView";
+        return "friends-view";
     }
 }
