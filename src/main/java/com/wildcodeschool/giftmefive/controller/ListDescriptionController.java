3a02e2898ab5c81d9152b63a258a9965443061b7
Marine Mombereau <Marinemombereau@gmail.com>
2019-10-25 08:29:20 +0200

    css réapparait dans listMaker

--- src/main/java/com/wildcodeschool/giftmefive/controller/ListDescriptionController.java
+++ src/main/java/com/wildcodeschool/giftmefive/controller/ListDescriptionController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ListDescriptionController {
     @GetMapping("/list-description")
     public String listDescription() {
-        return "/listMaker";
+        return "listMaker.html";
     }
 }
--- src/main/java/com/wildcodeschool/giftmefive/controller/ListGiftController.java
+++ src/main/java/com/wildcodeschool/giftmefive/controller/ListGiftController.java
@@ -3,18 +3,23 @@ package com.wildcodeschool.giftmefive.controller;
 import com.wildcodeschool.giftmefive.repository.ListGiftsRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ListGiftController {
-    @GetMapping("/register-list")
+    @PostMapping("/register-list")
     public String registerList
             (@RequestParam(name = "urlImage", required = false, defaultValue = "http") String urlImage,
-             @RequestParam(name = "listName", required = true) String listName,
-             @RequestParam(name = "descriptionList", required = true) String descriptionList) {
+             @RequestParam(name = "listName", required = false) String listName,
+             @RequestParam(name = "descriptionList", required = false) String descriptionList) {
         ListGiftsRepository repository = new ListGiftsRepository();
         ListGiftsRepository.registerInList(urlImage, listName, descriptionList);
-        return "/";
+        return "/index";
         // TODO Ajout Lien de la page de nouvelleListe
+    }
+    @GetMapping("/register-list")
+    public String showListForm(){
+        return "listMaker";
     }
 }
