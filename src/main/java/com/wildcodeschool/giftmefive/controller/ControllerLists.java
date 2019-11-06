e37d00fb4ff5083f2e4ea0ba5ca9cb1ea1cd8b17
Sebastien Ots <ots.sebastien@gmail.com>
2019-11-06 16:12:51 +0100

    supprimer liste

--- src/main/java/com/wildcodeschool/giftmefive/controller/ControllerLists.java
+++ src/main/java/com/wildcodeschool/giftmefive/controller/ControllerLists.java
@@ -4,15 +4,21 @@ import com.wildcodeschool.giftmefive.repository.ListsRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ControllerLists {
 
     private ListsRepository listsRepository = new ListsRepository();
-
     @GetMapping("/listes")
     public String getList(Model model) {
         model.addAttribute("lists", listsRepository.findAll());
         return "lists";
     }
+    @GetMapping("/listes/delete")
+    public String deleteListe(@RequestParam int id) {
+        listsRepository.deleteList(id);
+        return "redirect:/listes";
+    }
 }
+
