f1d3d41ddd392111317f7ab0bbb694cecdb63b81
Marie Vendran <marie.vendran@gmail.com>
2019-11-07 12:15:56 +0100

    as requested

--- src/main/java/com/wildcodeschool/giftmefive/controller/SaveGiftController.java
+++ src/main/java/com/wildcodeschool/giftmefive/controller/SaveGiftController.java
@@ -1,24 +1,11 @@
 package com.wildcodeschool.giftmefive.controller;
 
-import com.wildcodeschool.giftmefive.repository.ListGiftsRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
     public class SaveGiftController {
-    @PostMapping("/listes")
-    public String registerGift
-            (@RequestParam(name = "urlImage", required = false, defaultValue = "http") String urlImage,
-             @RequestParam(name = "urlArticle", required = false, defaultValue = "http") String urlArticle,
-             @RequestParam(name = "giftName", required = true) String giftName,
-             @RequestParam(name = "descriptionGift", required = true) String description,
-             @RequestParam(name = "price", required = true) int price) {
-        ListGiftsRepository repository = new ListGiftsRepository();
-        ListGiftsRepository.registerInList(urlImage, giftName, description);
-        return "/";
-    }
+
         @GetMapping("/description-cadeau")
         public String giftDescription() {
             return "giftMaker";
