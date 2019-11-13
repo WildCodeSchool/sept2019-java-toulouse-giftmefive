2b328629558c5c5d52a61599a879fad61cdd21a2
Sebastien Ots <ots.sebastien@gmail.com>
2019-11-13 10:23:08 +0100

    correction

--- src/main/java/com/wildcodeschool/giftmefive/controller/UserController.java
+++ src/main/java/com/wildcodeschool/giftmefive/controller/UserController.java
@@ -8,50 +8,52 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-    @Controller
-    public class UserController{
+import javax.servlet.http.HttpSession;
 
-        private UserRepository repository = new UserRepository();
+@Controller
+public class UserController {
 
-        @GetMapping("/identification")
-        public String callSign() {
-            return "signIn";
-        }
+    private UserRepository repository = new UserRepository();
 
-        @PostMapping("/connexion")
-        public String connexion(@RequestParam String username, @RequestParam String password) {
-            User user = repository.getByUsername(username, password);
-            if (user != null) {
-                return "redirect:/";
-            }
-            return "signIn";
-        }
 
-        @GetMapping("/inscription")
-        public String callSignIn() {
-            return "signUp";
-        }
-
-        @GetMapping("/modification")
-        public String callUserUdapte(Model out) {
-            //Todo methode recherche user connecté
-
-            return "user-update";
-        }
-
-        @PostMapping("/enregistre_user")
-        public String userUpdate(Model out,
-
-                                 @RequestParam Long idUser,
-                                 @RequestParam String username,
-                                 @RequestParam String password,
-                                 @RequestParam String passwordConfirmation,
-                                 @RequestParam String email
-        ) {
-            if (!password.equals(passwordConfirmation)) {
-                return "redirect:/modification";
-            }
-            out.addAttribute("user", repository.save(idUser, username, password, email));
+    @PostMapping("/connexion")
+    public String connexion(HttpSession session, @RequestParam String username, @RequestParam String password) {
+        User user = repository.getByUsername(username, password);
+        if (user != null) {
+            session.setAttribute("user", user);
             return "redirect:/";
         }
+        return "signIn";
     }
+
+    @GetMapping("/identification")
+    public String callSign() {
+        return "signIn";
+    }
+
+    @GetMapping("/inscription")
+    public String callSignIn() {
+        return "signUp.html";
+    }
+
+    @GetMapping("/modification")
+    public String callUserUdapte(Model out, @RequestParam Long id) {
+        out.addAttribute("User", repository.findById(id));
+        return "user-update";
+    }
+
+    @PostMapping("/enregistre_user")
+    public String userUpdate(Model out,
+                             @RequestParam Long idUser,
+                             @RequestParam String username,
+                             @RequestParam String password,
+                             @RequestParam String passwordConfirmation,
+                             @RequestParam String email
+    ) {
+        if (!password.equals(passwordConfirmation)) {
+            return "redirect:/modification?id=" + idUser;
+        }
+        out.addAttribute("user", repository.save(idUser, username, password, email));
+        return "redirect:/modification?id=" + idUser;
+    }
+}
