a7c277385a4be571428e79ccf5b8951a3b5767d3
bastienwcs <email>
2020-01-10 11:40:59 +0100

    configuration deploiement

--- src/main/java/com/wildcodeschool/giftmefive/GiftMeFiveApplication.java
+++ src/main/java/com/wildcodeschool/giftmefive/GiftMeFiveApplication.java
@@ -2,9 +2,10 @@ package com.wildcodeschool.giftmefive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GiftMeFiveApplication {
+public class GiftMeFiveApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(GiftMeFiveApplication.class, args);
